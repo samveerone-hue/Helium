d1756ecdc7bd5285ffa1f0402cbcec42c82a5142
LETd <samveerone@gmail.com>
2026-09-01 19:24:39 +0100

    Remove obsolete pre-1.21.11 camera reflection fallback

--- src/client/java/com/helium/util/VersionCompat.java
+++ src/client/java/com/helium/util/VersionCompat.java
@@ -4,12 +4,13 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 
-import java.lang.reflect.Method;
-
+/**
+ * Small compatibility surface for the dedicated Minecraft 1.21.11 build.
+ *
+ * <p>This branch targets 1.21.11 only, so there is no reason to retain
+ * intermediary-name reflection fallbacks for pre-1.21.11 camera APIs.</p>
+ */
 public final class VersionCompat {
-
-    private static Method legacyCameraPos = null;
-    private static boolean cameraFallbackResolved = false;
 
     private VersionCompat() {}
 
@@ -18,33 +19,6 @@ public final class VersionCompat {
     }
 
     public static Vec3d getCameraPosition(Camera camera) {
-        try {
-            return camera.getCameraPos();
-        } catch (NoSuchMethodError e) {
-            return getCameraPositionLegacy(camera);
-        }
-    }
-
-    private static Vec3d getCameraPositionLegacy(Camera camera) {
-        if (!cameraFallbackResolved) {
-            cameraFallbackResolved = true;
-            try {
-                String mapped = net.fabricmc.loader.api.FabricLoader.getInstance()
-                        .getMappingResolver()
-                        .mapMethodName("intermediary",
-                                "net.minecraft.class_4184",
-                                "method_19326",
-                                "()Lnet/minecraft/class_243;");
-                legacyCameraPos = Camera.class.getMethod(mapped);
-            } catch (Throwable ignored) {}
-        }
-
-        if (legacyCameraPos != null) {
-            try {
-                return (Vec3d) legacyCameraPos.invoke(camera);
-            } catch (Throwable ignored) {}
-        }
-
-        return Vec3d.ZERO;
+        return camera.getCameraPos();
     }
 }
