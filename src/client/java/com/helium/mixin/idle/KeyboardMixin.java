4bbc3d06bc3b422dfc95f37ea4f92b2d346e744e
LETd <samveerone@gmail.com>
2026-09-04 19:03:23 +0100

    cleanup: remove unused mixin stubs and particle accessor

--- src/client/java/com/helium/mixin/idle/KeyboardMixin.java
+++
@@ -1,8 +0,0 @@
-package com.helium.mixin.idle;
-
-import net.minecraft.client.Keyboard;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Keyboard.class)
-public abstract class KeyboardMixin {
-}
