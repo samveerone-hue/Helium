868e3099f07939f76c8a0f10de026fac1e9b3911
LETd <samveerone@gmail.com>
2026-09-04 19:29:04 +0100

    feat: support NVIDIA ARB parallel shader compilation fallback

--- src/client/java/com/helium/gpu/NvidiaOptimizer.java
+++ src/client/java/com/helium/gpu/NvidiaOptimizer.java
@@ -3,11 +3,13 @@ package com.helium.gpu;
 import com.helium.HeliumClient;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.KHRParallelShaderCompile;
+import org.lwjgl.opengl.ARBParallelShaderCompile;
 
 public final class NvidiaOptimizer {
 
     private static volatile boolean initialized = false;
     private static volatile boolean parallelShaderCompile = false;
+    private static volatile String shaderCompileExtension = "none";
 
     private NvidiaOptimizer() {}
 
@@ -21,10 +23,17 @@ public final class NvidiaOptimizer {
         }
 
         try {
-            if (GL.getCapabilities().GL_KHR_parallel_shader_compile) {
+            var caps = GL.getCapabilities();
+            if (caps.GL_KHR_parallel_shader_compile) {
                 KHRParallelShaderCompile.glMaxShaderCompilerThreadsKHR(0xFFFFFFFF);
                 parallelShaderCompile = true;
-                HeliumClient.LOGGER.info("nvidia: parallel shader compile enabled");
+                shaderCompileExtension = "KHR";
+                HeliumClient.LOGGER.info("nvidia: KHR parallel shader compile enabled");
+            } else if (caps.GL_ARB_parallel_shader_compile) {
+                ARBParallelShaderCompile.glMaxShaderCompilerThreadsARB(0xFFFFFFFF);
+                parallelShaderCompile = true;
+                shaderCompileExtension = "ARB";
+                HeliumClient.LOGGER.info("nvidia: ARB parallel shader compile enabled");
             }
         } catch (Throwable t) {
             HeliumClient.LOGGER.warn("nvidia: parallel shader compile unavailable", t);
@@ -39,5 +48,9 @@ public final class NvidiaOptimizer {
 
     public static boolean hasParallelShaderCompile() {
         return parallelShaderCompile;
+    }
+
+    public static String getShaderCompileExtension() {
+        return shaderCompileExtension;
     }
 }
