b90b2bf104ae1ca6f7ab87bc9ba7301deb1a05d4
LETd <samveerone@gmail.com>
2026-09-09 19:37:24 +0100

    Guard OpenGL-specific code from the 26.2 Vulkan backend

--- src/client/java/com/helium/mixin/render/GlStateManagerMixin.java
+++ src/client/java/com/helium/mixin/render/GlStateManagerMixin.java
@@ -3,6 +3,7 @@ package com.helium.mixin.render;
 import com.helium.HeliumClient;
 import com.helium.render.GLStateCache;
 import com.helium.render.ShaderUniformCache;
+import com.helium.platform.RenderBackend;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +15,7 @@ public abstract class GlStateManagerMixin {
 
     @Inject(method = "_activeTexture", at = @At("HEAD"), remap = false)
     private static void helium$trackActiveTexture(int texture, CallbackInfo ci) {
-        if (GLStateCache.isInitialized() && HeliumClient.getConfig().glStateCache) {
+        if (RenderBackend.isOpenGL() && GLStateCache.isInitialized() && HeliumClient.getConfig().glStateCache) {
             GLStateCache.setActiveTexture(texture);
         }
     }
