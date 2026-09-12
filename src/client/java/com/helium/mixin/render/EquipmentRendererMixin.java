381b391a96bca3f0fea9601f1007c4bbf63c56d7
LETd <samveerone@gmail.com>
2026-09-12 17:11:27 +0100

    fix: import equipment context from Rentities package

--- src/client/java/com/helium/mixin/render/EquipmentRendererMixin.java
+++ src/client/java/com/helium/mixin/render/EquipmentRendererMixin.java
@@ -1,6 +1,7 @@
 package com.helium.mixin.render;
 
 import com.helium.rentities.entities.RentitiesEquipmentBatcher;
+import com.helium.rentities.entities.RentitiesEquipmentContext;
 import net.minecraft.client.model.Model;
 import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.entity.equipment.EquipmentModel;
