e63ac179e7e2ecc70cd989839ba0450a1a9cee84
LETd <samveerone@gmail.com>
2026-09-12 11:51:59 +0100

    fix: restore ClientWorld mixin injection import

--- src/client/java/com/helium/mixin/tick/ClientWorldMixin.java
+++ src/client/java/com/helium/mixin/tick/ClientWorldMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientWorld.class)
