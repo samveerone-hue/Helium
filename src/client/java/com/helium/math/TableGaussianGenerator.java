39a88f1d7576fc1a3ce978b439d34ab9bf56c13c
LETd <samveerone@gmail.com>
2026-09-13 21:48:48 +0100

    fix: use exact math in Gaussian ziggurat recurrence

--- src/client/java/com/helium/math/TableGaussianGenerator.java
+++ src/client/java/com/helium/math/TableGaussianGenerator.java
@@ -1,9 +1,18 @@
 package com.helium.math;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.GaussianGenerator;
 import net.minecraft.util.math.random.Random;
 
+/**
+ * Ziggurat Gaussian generator.
+ *
+ * The table construction and tail acceptance use exact Math.log/Math.exp because the
+ * cheaper GBF approximations do not preserve the recurrence's positivity invariant.
+ * The fast-random toggle remains fail-closed until distribution/throughput validation
+ * is complete.
+ */
 public class TableGaussianGenerator extends GaussianGenerator {
 
     private static final int TABLE_SIZE = 256;
@@ -14,15 +23,16 @@ public class TableGaussianGenerator extends GaussianGenerator {
     private static final float[] Y = new float[TABLE_SIZE + 1];
 
     static {
-        float f = GBFMath.fastexp(-0.5F * R * R);
+        float f = (float) Math.exp(-0.5F * R * R);
         X[0] = R / f;
         X[1] = R;
         Y[0] = 0.0F;
         Y[1] = f;
 
         for (int i = 2; i <= TABLE_SIZE; i++) {
-            X[i] = MathHelper.sqrt(-2.0F * GBFMath.fastlog(GBFMath.fastexp(-0.5F * X[i - 1] * X[i - 1]) + Y[i - 1] / X[i - 1]));
-            Y[i] = GBFMath.fastexp(-0.5F * X[i] * X[i]);
+            X[i] = MathHelper.sqrt((float) (-2.0F * Math.log(
+                    Math.exp(-0.5F * X[i - 1] * X[i - 1]) + Y[i - 1] / X[i - 1])));
+            Y[i] = (float) Math.exp(-0.5F * X[i] * X[i]);
         }
     }
 
@@ -48,13 +58,14 @@ public class TableGaussianGenerator extends GaussianGenerator {
             if (i == 0) {
                 double xx, yy;
                 do {
-                    xx = -GBFMath.fastlog((float) this.baseRandom.nextDouble()) * INV_R;
-                    yy = -GBFMath.fastlog((float) this.baseRandom.nextDouble());
+                    xx = -Math.log(this.baseRandom.nextDouble()) * INV_R;
+                    yy = -Math.log(this.baseRandom.nextDouble());
                 } while (yy + yy < xx * xx);
                 return (this.baseRandom.nextBoolean() ? R + xx : -R - xx);
             }
 
-            if (Y[i + 1] + (Y[i] - Y[i + 1]) * this.baseRandom.nextDouble() < GBFMath.fastexp((float) (-0.5 * xVal * xVal))) {
+            if (Y[i + 1] + (Y[i] - Y[i + 1]) * this.baseRandom.nextDouble()
+                    < Math.exp(-0.5 * xVal * xVal)) {
                 return this.baseRandom.nextBoolean() ? xVal : -xVal;
             }
         }
