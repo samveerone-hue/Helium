98beed74cd825594d5c8aec2378d2f8206687b0b
LETd <samveerone@gmail.com>
2026-09-13 21:07:55 +0100

    cleanup: remove unused packet batcher implementation

--- src/client/java/com/helium/network/PacketBatcher.java
+++
@@ -1,6 +0,0 @@
-package com.helium.network;
-
-/**
- * Packet batching is implemented at the ClientConnection flush boundary.
- * This class was an unused alternate implementation and is intentionally absent.
- */
