cfa237e4680abb91fd0567f383ad72acd0658b88
LETd <samveerone@gmail.com>
2026-09-14 17:17:38 +0100

    fix: correct Gaussian ziggurat implementation for 26.1.2

--- src/client/java/com/helium/math/TableGaussianGenerator.java
+++ src/client/java/com/helium/math/TableGaussianGenerator.java
@@ -1,29 +1,36 @@
 package com.helium.math;
 
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.levelgen.MarsagliaPolarGaussian;
 import net.minecraft.util.RandomSource;
 
+/** Standard 128-layer Ziggurat normal generator. */
 public class TableGaussianGenerator extends MarsagliaPolarGaussian {
 
-    private static final int TABLE_SIZE = 256;
-    private static final float R = 3.442619855899F;
-    private static final float INV_R = 0.29047645161474317F;
+    private static final int TABLE_SIZE = 128;
+    private static final double R = 3.442619855899D;
+    private static final double AREA = 9.91256303526217E-3D;
+    private static final double INV_R = 1.0D / R;
 
-    private static final float[] X = new float[TABLE_SIZE + 1];
-    private static final float[] Y = new float[TABLE_SIZE + 1];
+    private static final double[] X = new double[TABLE_SIZE + 1];
+    private static final double[] F = new double[TABLE_SIZE + 1];
 
     static {
-        float f = GBFMath.fastexp(-0.5F * R * R);
-        X[0] = R / f;
+        double tailHeight = Math.exp(-0.5D * R * R);
+        X[0] = AREA / tailHeight;
         X[1] = R;
-        Y[0] = 0.0F;
-        Y[1] = f;
+        F[0] = 1.0D;
+        F[1] = tailHeight;
 
-        for (int i = 2; i <= TABLE_SIZE; i++) {
-            X[i] = Mth.sqrt(-2.0F * GBFMath.fastlog(GBFMath.fastexp(-0.5F * X[i - 1] * X[i - 1]) + Y[i - 1] / X[i - 1]));
-            Y[i] = GBFMath.fastexp(-0.5F * X[i] * X[i]);
+        double dn = R;
+        for (int i = 2; i < TABLE_SIZE; i++) {
+            dn = Math.sqrt(-2.0D * Math.log(
+                    AREA / dn + Math.exp(-0.5D * dn * dn)));
+            X[i] = dn;
+            F[i] = Math.exp(-0.5D * dn * dn);
         }
+
+        X[TABLE_SIZE] = 0.0D;
+        F[TABLE_SIZE] = 1.0D;
     }
 
     public TableGaussianGenerator(RandomSource rand) {
@@ -37,25 +44,26 @@ public class TableGaussianGenerator extends MarsagliaPolarGaussian {
     public double nextGaussian() {
         while (true) {
             int i = this.randomSource.nextInt(TABLE_SIZE);
-            long j = this.randomSource.nextInt() & 0xFFFFFFFFL;
+            double x = this.randomSource.nextDouble() * X[i];
 
-            double xVal = j * (X[i] / 4294967296.0);
-
-            if (Math.abs(xVal) < X[i + 1]) {
-                return xVal;
+            if (x < X[i + 1]) {
+                return this.randomSource.nextBoolean() ? x : -x;
             }
 
             if (i == 0) {
-                double xx, yy;
+                double xx;
+                double yy;
                 do {
-                    xx = -GBFMath.fastlog((float) this.randomSource.nextDouble()) * INV_R;
-                    yy = -GBFMath.fastlog((float) this.randomSource.nextDouble());
+                    xx = -Math.log(this.randomSource.nextDouble()) * INV_R;
+                    yy = -Math.log(this.randomSource.nextDouble());
                 } while (yy + yy < xx * xx);
-                return (this.randomSource.nextBoolean() ? R + xx : -R - xx);
+                double tail = R + xx;
+                return this.randomSource.nextBoolean() ? tail : -tail;
             }
 
-            if (Y[i + 1] + (Y[i] - Y[i + 1]) * this.randomSource.nextDouble() < GBFMath.fastexp((float) (-0.5 * xVal * xVal))) {
-                return this.randomSource.nextBoolean() ? xVal : -xVal;
+            if (F[i + 1] + (F[i] - F[i + 1]) * this.randomSource.nextDouble()
+                    < Math.exp(-0.5D * x * x)) {
+                return this.randomSource.nextBoolean() ? x : -x;
             }
         }
     }
